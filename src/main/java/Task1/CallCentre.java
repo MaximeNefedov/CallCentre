55badce8a54991ea6858338d49c469a741d0fbf2
Maxim Nefedov <max-ipad@mail.ru>
2020-12-20 21:01:32 +0300

    Initial commit

---
+++ src/main/java/Task1/CallCentre.java
@@ -0,0 +1,48 @@
+package Task1;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class CallCentre {
+    private int counter;
+    private static final int DELAY = 1000;
+    private static final int WORKING_PROCESS = 3000;
+
+//    Я принял решение использовать реализацию ArrayBlockingQueue из-за возможности соблюдения честной блокировки
+//    Данный колл-центр работает по принципу многих серверов, которые предустанавливают лимит на
+//    максимальное количество пользователей, которые могут находиться в режиме ожидания
+
+    private BlockingQueue<String> queue = new ArrayBlockingQueue<>(10);
+
+    public CallCentre(int counter) {
+        this.counter = counter;
+    }
+
+    public void handleCall() {
+        while (counter != 0) {
+            try {
+                String person = queue.take();
+                System.out.printf("%s начал обработку звонка %s\n", Thread.currentThread().getName(), person);
+                Thread.sleep(WORKING_PROCESS);
+                System.out.printf("%s закончил обработку звонка %s\n", Thread.currentThread().getName(), person);
+            } catch (InterruptedException e) {
+                System.out.printf("%s ушел домой\n", Thread.currentThread().getName());
+            }
+        }
+    }
+
+    public void generateCall() {
+        while (counter != 0) {
+            try {
+                Thread.sleep(DELAY);
+                String personName = "Абонент " + counter;
+                System.out.println(personName + " совершает звонок");
+                queue.put(personName);
+                counter--;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
---
+++ src/main/java/Task1/Main.java
@@ -0,0 +1,22 @@
+package Task1;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        CallCentre centre = new CallCentre(5);
+        Thread generator = new Thread(centre::generateCall);
+
+        Thread operator1 = new Thread(centre::handleCall, "Оператор 1");
+        Thread operator2 = new Thread(centre::handleCall, "Оператор 2");
+        Thread operator3 = new Thread(centre::handleCall, "Оператор 3");
+
+        generator.start();
+        operator1.start();
+        operator2.start();
+        operator3.start();
+
+        generator.join();
+        operator1.join();
+        operator2.join();
+        operator3.join();
+    }
+}
